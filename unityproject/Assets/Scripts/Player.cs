03752bf16b75728a0928eb98843e7cb3711f7331
tferrerITBA <tferrer@itba.edu.ar>
2020-10-10 18:08:14 -0300

    Add basic player movement

---
+++ unityproject/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed;
+    
+    private CharacterController _characterController;
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float vertical = Input.GetAxis("Vertical") * movementSpeed;
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        
+        Vector3 move = new Vector3(vertical, 0, -1 * horizontal);
+        _characterController.Move(move * Time.deltaTime);
+    }
+}
