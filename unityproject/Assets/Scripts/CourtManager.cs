671f35f97eb68f9f3226b77f3b5c515f0edc1854
mlund <mlund@itba.edu.ar>
2020-10-12 17:31:30 -0300

    Add court manager

---
+++ unityproject/Assets/Scripts/CourtManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CourtManager : MonoBehaviour
+{
+    private const float TotalCourtWidth = 18.0f;
+    private const float CourtLength = 39.0f;
+    private const float ServingAreaWidth = 13.5f;
+    private const float ServingAreaLength = 21.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private enum CourtSection
+    {
+        
+    }
+}
