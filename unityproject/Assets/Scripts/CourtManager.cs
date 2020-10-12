721948d472ee7354976fe491c1b575d8eee0988c
tferrerITBA <tferrer@itba.edu.ar>
2020-10-12 17:38:30 -0300

    Create court sections

--- unityproject/Assets/Scripts/CourtManager.cs
+++ unityproject/Assets/Scripts/CourtManager.cs
@@ -8,6 +8,18 @@ public class CourtManager : MonoBehaviour
     private const float CourtLength = 39.0f;
     private const float ServingAreaWidth = 13.5f;
     private const float ServingAreaLength = 21.0f;
+    
+    public enum CourtSection
+    {
+        Player1NoMansLand,
+        Player1LeftServiceBox,
+        Player1RightServiceBox,
+        Player2NoMansLand,
+        Player2LeftServiceBox,
+        Player2RightServiceBox
+    }
+    
+    public Dictionary<CourtSection, Vector2> courtSections = new Dictionary<CourtSection,Vector2>();
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +29,6 @@ public class CourtManager : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
-
-    private enum CourtSection
     {
         
     }
