02d67cb9a2c74e45efea11319c9cce47559d3864
tferrerITBA <tferrer@itba.edu.ar>
2020-11-11 16:21:52 -0300

    Finish Pause menu

--- unityproject/Assets/Scripts/GameManager.cs
+++ unityproject/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class GameManager : MonoBehaviour
     [HideInInspector] public SoundManager soundManager;
     public AIPlayer aiPlayer;
     public Player player;
+
+    public GameObject pauseMenu;
+    [HideInInspector] public bool canOpenPauseMenu = true;
     
 
     private void Awake()
@@ -28,7 +32,28 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape) && canOpenPauseMenu)
+        {
+            Time.timeScale = pauseMenu.activeSelf ? 1 : 0;
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            AudioListener.pause = pauseMenu.activeSelf;
+            player._movementBlocked = !pauseMenu.activeSelf;
+        }
+
+    }
+    
+    public void PauseMenuResume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+    
+    public void GoToMainMenu()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(0);
     }
 
     public void PlayerHitBall(Vector3 startPos, Vector3 ballTargetPos)
