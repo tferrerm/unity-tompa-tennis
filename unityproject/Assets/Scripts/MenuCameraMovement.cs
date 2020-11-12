288e9a3ef5c15e6f3d12bc22347fbe6ea46ccf7e
mlund <mlund@itba.edu.ar>
2020-11-12 16:11:11 -0300

    Add menu dynamic background

---
+++ unityproject/Assets/Scripts/MenuCameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCameraMovement : MonoBehaviour
+{
+    public Transform startPos;
+    public Transform endPos;
+    private float _t = 0f;
+    [Range(0,0.001f)] public float deltaT = 0.01f;
+    private bool _movingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_movingRight)
+        {
+            if (_t + deltaT > 1)
+            {
+                _t = 1;
+                _movingRight = false;
+            }
+            else
+            {
+                _t += deltaT;
+            }
+        }
+        else
+        {
+            if (_t - deltaT < 0)
+            {
+                _t = 0;
+                _movingRight = true;
+            } else
+            {
+                _t -= deltaT;
+            }
+        }
+        transform.position = Vector3.Lerp(startPos.position, endPos.position, _t);
+    }
+}
