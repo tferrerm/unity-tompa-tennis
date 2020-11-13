da651ae4fff3a70ffaec4e2f1c65f4df6e534fed
tferrerITBA <tferrer@itba.edu.ar>
2020-11-13 15:03:07 -0300

    Add UI for selecting sets and player name

---
+++ unityproject/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject mainMenu;
+    public GameObject playMenu;
+    public GameObject optionsMenu;
+    
+    public void OpenPlayMenu()
+    {
+        title.SetActive(false);
+        mainMenu.SetActive(false);
+        playMenu.SetActive(true);   
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ unityproject/Assets/Scripts/UI/PlayMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayMenu : MonoBehaviour
+{
+    public GameObject title;
+    public GameObject mainMenu;
+    public GameObject playMenu;
+
+    public TMP_InputField playerNameInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNameInput.text = PlayerPrefs.GetString("PlayerName", playerNameInput.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPrefs.SetString("PlayerName", playerNameInput.text);
+    }
+    
+    public void ReturnToMainMenu()
+    {
+        playMenu.SetActive(false);
+        title.SetActive(true);
+        mainMenu.SetActive(true);
+    }
+    
+    public void PlayGame()
+    {
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
