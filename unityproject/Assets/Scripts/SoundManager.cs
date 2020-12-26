9bff50bb6b707484b8d5e4dcf09fb09f96a33266
tferrerITBA <tferrer@itba.edu.ar>
2020-12-25 21:34:07 -0300

    Add macOS build

--- unityproject/Assets/Scripts/SoundManager.cs
+++ unityproject/Assets/Scripts/SoundManager.cs
@@ -82,7 +82,7 @@ public class SoundManager : MonoBehaviour
 
     public void PlayReplayMusic()
     {
-        replayAudioSource.volume = 0.3f;
+        replayAudioSource.volume = 0.5f;
         replayAudioSource.PlayOneShot(replayMusic);
     }
 
